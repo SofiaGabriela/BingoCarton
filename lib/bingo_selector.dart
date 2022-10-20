3405bca3605d18d400b7bbf8d1377c3eea06c2ea
SofiaGabriela <112833789+SofiaGabriela@users.noreply.github.com>
2022-10-20 11:08:52 -0300

    random list - manual list

--- lib/bingo_selector.dart
+++ lib/bingo_selector.dart
@@ -1,3 +1,4 @@
+import 'package:bingo_selector/custom_button_component.dart';
 import 'package:flutter/material.dart';
 
 class BingoModel{
@@ -12,8 +13,8 @@ class BingoModel{
 }
 
 enum Options {
-  selected,
-  notSelected,
+  manual,
+  random,
 }
 
 class BingoCarton extends StatefulWidget {
@@ -27,53 +28,92 @@ class BingoCarton extends StatefulWidget {
 
 class _BingoCartonState extends State<BingoCarton> {
 
-  Options currentOption = Options.selected;
+  Options currentOption = Options.manual;
   List<Widget> bingoCards = [];
+  List<BingoModel> randomBingoCards = [];
   List<int> selectedBingos = [];
   ScrollController scrollController = ScrollController();
 
   void addBingo(BingoModel bingo){
     setState(() {
-      selectedBingos.add(bingo.id);
+      if(selectedBingos.contains(bingo.id )){selectedBingos.remove(bingo.id); }
+      else { selectedBingos.add(bingo.id);}
     });
   }
 
-  @override
-  void initState() {
-    widget.list.forEach((element) {
-      bingoCards.add(_bingoButton(element));
+  void onChangeOption(Options option){
+    setState(() {
+      selectedBingos = [];
+      currentOption = option;
+      if (option == Options.random){
+        randomBingoCards = [];
+        getRandomList(3, widget.list);
+        randomBingoCards.forEach((element) {selectedBingos.add(element.id);});
+      }
     });
-    super.initState();
   }
 
+  void getRandomList(int n, List<BingoModel> bingos){
+    List<BingoModel> list = [...bingos];
+    BingoModel randomBingo;
+
+    for(int i=0; i<n; i++){
+      randomBingo = ((list..shuffle()).first);
+      randomBingoCards.add(randomBingo);
+      list.remove(randomBingo);
+    }
+  }
+
+  List<BingoModel> getList(Options option){
+    if(option == Options.manual){
+      return widget.list;
+    }
+    else {
+      return randomBingoCards;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    bingoCards = [];
+
+    getList(currentOption).forEach((element) {
+      bingoCards.add(_bingoButton(element));
+    });
+
     return Container(
       width: 320,
       height: 500,
       color: const Color(0xffF5F5F5),
       child: Column(
         children: [
-          const Align( alignment: Alignment.topLeft,
-              child: Text('Comprar',
-                style: TextStyle(color: Colors.black, fontSize: 14, fontWeight: FontWeight.bold, letterSpacing: 1.0),)),
-          const SizedBox(height: 15,),
-          const Align( alignment: Alignment.topLeft,
-              child: Text('Seleccione una de las siguientes opciones: ',
-                style: TextStyle(color: Colors.black, fontSize: 12, fontWeight: FontWeight.normal, letterSpacing: 0.5),)),
+          const SizedBox(
+            width: 320, height: 35,
+            child: Align( alignment: Alignment.topLeft,
+                child: Text('Comprar',
+                  style: TextStyle(color: Colors.black, fontSize: 14, fontWeight: FontWeight.bold, letterSpacing: 1.0),)),
+          ),
+          const SizedBox(
+            width: 320,
+            child: Align( alignment: Alignment.topLeft,
+                child: Text('Seleccione una de las siguientes opciones: ',
+                  style: TextStyle(color: Colors.black, fontSize: 12, fontWeight: FontWeight.normal, letterSpacing: 0.5),)),
+          ),
           const SizedBox(height: 20,),
-          Row( children: [
-            _optionButtons('Elegir mi cartón', Options.selected),
-            const SizedBox(width: 15,),
-            _optionButtons('Selección aleatoria', Options.notSelected),
+          SizedBox(
+            width: 320,
+            child: Row( children: [
+              _optionButtons('Elegir mi cartón', Options.manual),
+              const SizedBox(width: 15,),
+              _optionButtons('Selección aleatoria', Options.random),
         ],),
+          ),
           const SizedBox(height: 15,),
           _bingoCardList(),
           const SizedBox(height: 10,),
           _counter(),
           const SizedBox(height: 15,),
-          const SizedBox(width: 320, height: 20,)
+          CustomButton(text: 'Pagar', textSize: 13,)
         ]
       ),
     );
@@ -83,16 +123,16 @@ class _BingoCartonState extends State<BingoCarton> {
     return SizedBox(
       child: Row(children: [
         InkWell(
-          onTap: (){
-            setState(() {
-              currentOption = option;
-            });
-          },
+          onTap: (){ onChangeOption(option); },
           child: Stack(
             alignment: Alignment.center,
             children: [
-              Container(width: 25, height: 25,
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(100), color: Colors.white),),
+              Material(
+                elevation: 3,
+                borderRadius: BorderRadius.circular(100),
+                child: Container(width: 25, height: 25,
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(100), color: Colors.white),),
+              ),
               Container(height: 16, width: 16,
                 decoration:  BoxDecoration( color: option == currentOption ? widget.color : Colors.white, borderRadius: BorderRadius.circular(100)),),
             ]
@@ -134,14 +174,14 @@ class _BingoCartonState extends State<BingoCarton> {
   }
 
   Widget _bingoButton(BingoModel bingo){
-    bool isSelected = false;
+    bool isSelected = selectedBingos.contains(bingo.id);
 
     return InkWell(
       onTap: (){
         addBingo(bingo);
-        isSelected = selectedBingos.contains(bingo.id);
         },
-      child: Container(
+      child: AnimatedContainer(
+        duration: const Duration (milliseconds: 200),
         width: 150,
         height: 33,
         decoration: BoxDecoration(
@@ -158,7 +198,6 @@ class _BingoCartonState extends State<BingoCarton> {
     );
   }
 
-  // width: 320, height: 40
   Widget _counter(){
     return Material(
       elevation: 1,
--- lib/main.dart
+++ lib/main.dart
@@ -29,23 +29,28 @@ class _TestMainState extends State<TestMain> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: BingoCarton(
-        list: [
-          BingoModel(id: 1, number: 100045),
-          BingoModel(id: 2, number: 100044),
-          BingoModel(id: 3, number: 100043),
-          BingoModel(id: 4, number: 100023),
-          BingoModel(id: 5, number: 100045),
-          BingoModel(id: 6, number: 100044),
-          BingoModel(id: 7, number: 100044),
-          BingoModel(id: 8, number: 100045),
-          BingoModel(id: 9, number: 100044),
-          BingoModel(id: 10, number: 100043),
-          BingoModel(id: 11, number: 100023),
-          BingoModel(id: 12, number: 100045),
-          BingoModel(id: 13, number: 100044),
-          BingoModel(id: 14, number: 100044),
-          ],
+      body: Center(child: Container(
+        width: 370,
+        height: 500,
+        color: const Color(0xffF5F5F5),
+        child: BingoCarton(
+          list: [
+            BingoModel(id: 1, number: 100041),
+            BingoModel(id: 2, number: 100042),
+            BingoModel(id: 3, number: 100043),
+            BingoModel(id: 4, number: 100024),
+            BingoModel(id: 5, number: 100045),
+            BingoModel(id: 6, number: 100046),
+            BingoModel(id: 7, number: 100047),
+            BingoModel(id: 8, number: 100048),
+            BingoModel(id: 9, number: 100049),
+            BingoModel(id: 10, number: 100040),
+            BingoModel(id: 11, number: 100021),
+            BingoModel(id: 12, number: 100022),
+            BingoModel(id: 13, number: 100023),
+            BingoModel(id: 14, number: 100029),
+            ],
+        ),
       )
       ),
     );
