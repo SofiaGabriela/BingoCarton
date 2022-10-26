be4e28dfea9a4fc91694601d04707efd6d0101d8
Gut333 <107408760+gut333@users.noreply.github.com>
2022-10-26 19:21:53 -0300

    fixed variable names

--- lib/bingo_selector.dart
+++ lib/bingo_selector.dart
@@ -1,33 +1,37 @@
+// ignore_for_file: must_be_immutable
 import 'dart:math';
 import 'package:flutter/material.dart';
 
-class BingoModel{
-  int id;
-  int number;
-
-  BingoModel({required this.id, required this.number});
-
-  int get gedId => id;
-  int get getNumber => number;
-}
-
-enum Options {
-  manual,
-  random,
-}
-
 class BingoCarton extends StatefulWidget {
-  int maxAmount;
+  int maxQuantity;
   List<BingoModel> list;
   Color color;
   Function(List<int>)? onBuy;
+
+  ///==========Bingo Selector==========
+  ///
+  ///Integrantes:
+  ///
+  ///-Marín, Sofía
+  ///
+  ///-Reynaga, Rodrigo
+  ///
+  ///-Narvaez, Gustavo
+  ///
+  ///Con este widget se muestran los números de los cartones de un bingo disponibles
+  ///para comprar. Por defecto se pueden elegir 3 cartones de forma manual o al azar,
+  ///pudiendo modificarse esta cantidad si es necesario. Tener en cuenta esta cantidad
+  ///no tiene que superar el número total de cartones.
+  ///
+  ///Tambien se puede customizar el color principal que llevarán los cartones, el
+  ///selector y el botón para realizar el pago.
 
   BingoCarton({
     Key? key,
     required this.list,
     this.color = const Color(0xff0000b2),
     this.onBuy,
-    this.maxAmount = 3,
+    this.maxQuantity = 3,
   }) : super(key: key);
 
   @override
@@ -41,11 +45,13 @@ class _BingoCartonState extends State<BingoCarton> {
   List<int> selectedBingos = [];
   ScrollController scrollController = ScrollController();
 
-  void addBingo(BingoModel bingo){
+  void addBingo(BingoModel bingo) {
     setState(() {
       if (selectedBingos.contains(bingo.id)) {
         selectedBingos.remove(bingo.id);
-      } else if (selectedBingos.length < widget.maxAmount) { selectedBingos.add(bingo.id); }
+      } else if (selectedBingos.length < widget.maxQuantity) {
+        selectedBingos.add(bingo.id);
+      }
     });
   }
 
@@ -53,35 +59,37 @@ class _BingoCartonState extends State<BingoCarton> {
     setState(() {
       selectedBingos = [];
       currentOption = option;
-      if (option == Options.random){
+      if (option == Options.random) {
         randomBingoCards = [];
         randomBingoCards = getRandomList();
-        randomBingoCards.forEach((element) {addBingo(element);});
+        randomBingoCards.forEach((element) {
+          addBingo(element);
+        });
       }
     });
   }
 
-  List<BingoModel> getRandomList( ){
+  List<BingoModel> getRandomList() {
     final random = Random();
     List<BingoModel> list = [];
     int aux = 0;
-    int cantidad = widget.maxAmount;
-    int tope = widget.list.length;
-    for(int i=0; i< cantidad; i++ ){
-      aux = random.nextInt(tope);
-      while(list.contains(widget.list[aux])){
-        aux = random.nextInt(tope);
+    int quantity = widget.maxQuantity;
+    int limit = widget.list.length;
+    for (int i = 0; i < quantity; i++) {
+      aux = random.nextInt(limit);
+      while (list.contains(widget.list[aux])) {
+        aux = random.nextInt(limit);
       }
       list.add(widget.list[aux]);
     }
     return list;
-
   }
 
   @override
   Widget build(BuildContext context) {
     bingoCards = [];
-    (currentOption == Options.manual? widget.list : randomBingoCards).forEach((element) {
+    (currentOption == Options.manual ? widget.list : randomBingoCards)
+        .forEach((element) {
       bingoCards.add(_bingoButton(element));
     });
 
@@ -89,107 +97,166 @@ class _BingoCartonState extends State<BingoCarton> {
       width: 320,
       height: 500,
       color: const Color(0xffF5F5F5),
-      child: Column(
-        children: [
-          _text(),
-          const SizedBox(height: 20,),
-          SizedBox(
-            width: 320,
-            child: Row( children: [
+      child: Column(children: [
+        _header(),
+        const SizedBox( height: 20 ),
+        SizedBox(
+          width: 320,
+          child: Row(
+            children: [
               _optionButtons('Elegir mi cartón', Options.manual),
-              const SizedBox(width: 15,),
+              const SizedBox(
+                width: 15,
+              ),
               _optionButtons('Selección aleatoria', Options.random),
-        ],),
+            ],
           ),
-          _pagesControl()
-        ]
+        ),
+        _pagesControl()
+      ]),
+    );
+  }
+
+  _header() {
+    return Column(children: const [
+      SizedBox(
+        width: 320,
+        height: 35,
+        child: Align(
+            alignment: Alignment.topLeft,
+            child: Text(
+              'Comprar',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 1.0),
+            )),
+      ),
+      SizedBox(
+        width: 320,
+        child: Align(
+            alignment: Alignment.topLeft,
+            child: Text(
+              'Seleccione una de las siguientes opciones: ',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 12,
+                  fontWeight: FontWeight.normal,
+                  letterSpacing: 0.5),
+            )),
+      ),
+    ]);
+  }
+
+  Widget _optionButtons(String text, Options option) {
+    return SizedBox(
+      child: Row(
+        children: [
+          InkWell(
+            onTap: () {
+              onChangeOption(option);
+            },
+            child: Stack(alignment: Alignment.center, children: [
+              Material(
+                elevation: 3,
+                borderRadius: BorderRadius.circular(100),
+                child: Container(
+                  width: 25,
+                  height: 25,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100),
+                      color: Colors.white),
+                ),
+              ),
+              Container(
+                height: 16,
+                width: 16,
+                decoration: BoxDecoration(
+                    color:
+                        option == currentOption ? widget.color : Colors.white,
+                    borderRadius: BorderRadius.circular(100)),
+              ),
+            ]),
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          Text(text,
+              style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 12,
+                  fontWeight: FontWeight.normal,
+                  letterSpacing: 0.5))
+        ],
       ),
     );
   }
 
-  _text(){
-    return
-      Column(
-      children : const [
-        SizedBox(
-        width: 320, height: 35,
-        child: Align( alignment: Alignment.topLeft,
-            child: Text('Comprar',
-              style: TextStyle(color: Colors.black, fontSize: 14, fontWeight: FontWeight.bold, letterSpacing: 1.0),)),
-      ),
-       SizedBox(
-        width: 320,
-        child: Align( alignment: Alignment.topLeft,
-            child: Text('Seleccione una de las siguientes opciones: ',
-              style: TextStyle(color: Colors.black, fontSize: 12, fontWeight: FontWeight.normal, letterSpacing: 0.5),)),
-      ),
-        ]
-      );
-  }
-
-  Widget _optionButtons(String text, Options option){
-    return SizedBox(
-      child: Row(children: [
-        InkWell(
-          onTap: (){ onChangeOption(option); },
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              Material(
-                elevation: 3,
-                borderRadius: BorderRadius.circular(100),
-                child: Container(width: 25, height: 25,
-                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(100), color: Colors.white),),
-              ),
-              Container(height: 16, width: 16,
-                decoration:  BoxDecoration( color: option == currentOption ? widget.color : Colors.white, borderRadius: BorderRadius.circular(100)),),
-            ]
-          ),
-        ),
-        const SizedBox(width: 10,),
-        Text(text, style: const TextStyle(color: Colors.black, fontSize: 12, fontWeight: FontWeight.normal, letterSpacing: 0.5))
-      ],),);
-  }
-
-  Widget _pagesControl(){
+  Widget _pagesControl() {
     return IndexedStack(
       index: currentOption.index,
+      children: [_manualOptionPage(), _randomOptionPage()],
+    );
+  }
+
+  Widget _manualOptionPage() {
+    return SizedBox(
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 15,
+          ),
+          _bingoCardList(),
+          const SizedBox(
+            height: 10,
+          ),
+          _counter(),
+          const SizedBox(
+            height: 15,
+          ),
+          CustomButton(
+            text: 'Pagar',
+            backgroundColor: widget.color,
+            textSize: 13,
+            isEnabled: selectedBingos.isNotEmpty,
+            onTap: () {
+              widget.onBuy!(selectedBingos);
+            },
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _randomOptionPage() {
+    return Column(
       children: [
-        _manualOptionPage(),
-        _randomOptionPage()
+        const SizedBox(
+          height: 15,
+        ),
+        Wrap(
+          direction: Axis.horizontal,
+          spacing: 15,
+          runSpacing: 15,
+          children: bingoCards,
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+        CustomButton(
+            text: 'Pagar',
+            backgroundColor: widget.color,
+            textSize: 13,
+            isEnabled: selectedBingos.isNotEmpty,
+            onTap: () {
+              widget.onBuy!(selectedBingos);
+            })
       ],
     );
   }
 
-  Widget _manualOptionPage(){
-    return SizedBox( child: Column( children: [
-        const SizedBox(height: 15,),
-        _bingoCardList(),
-        const SizedBox(height: 10,),
-        _counter(),
-        const SizedBox(height: 15,),
-        CustomButton(text: 'Pagar',backgroundColor: widget.color, textSize: 13, isEnabled: selectedBingos.isNotEmpty,
-        onTap: (){ widget.onBuy!(selectedBingos);  } ,)],
-      ),
-      );
-}
-
-  Widget _randomOptionPage(){
-    return Column( children: [
-      const SizedBox(height: 15,),
-      Wrap(
-      direction: Axis.horizontal,
-      spacing: 15,
-      runSpacing: 15,
-      children: bingoCards,),
-      const SizedBox(height: 15,),
-      CustomButton(text: 'Pagar',backgroundColor: widget.color, textSize: 13, isEnabled: selectedBingos.isNotEmpty,
-          onTap: (){ widget.onBuy!(selectedBingos);  } )
-    ],
-    );
-  }
-
-  Widget _bingoCardList(){
+  Widget _bingoCardList() {
     return SizedBox(
       width: 320,
       height: 280,
@@ -200,11 +267,11 @@ class _BingoCartonState extends State<BingoCarton> {
         thumbColor: Colors.transparent,
         radius: const Radius.circular(5),
         child: Theme(
-          data:ThemeData(
+          data: ThemeData(
             scrollbarTheme: const ScrollbarThemeData(
-              //thickness: MaterialStatePropertyAll(4),
-              //thumbVisibility: MaterialStatePropertyAll(false),
-            ),
+                //thickness: MaterialStatePropertyAll(4),
+                //thumbVisibility: MaterialStatePropertyAll(false),
+                ),
           ),
           child: SingleChildScrollView(
             controller: scrollController,
@@ -220,53 +287,83 @@ class _BingoCartonState extends State<BingoCarton> {
     );
   }
 
-  Widget _bingoButton(BingoModel bingo){
+  Widget _bingoButton(BingoModel bingo) {
     bool isSelected = selectedBingos.contains(bingo.id);
 
     return InkWell(
-      onTap: (){
+      onTap: () {
         addBingo(bingo);
-        },
+      },
       child: AnimatedContainer(
-        duration: const Duration (milliseconds: 200),
+        duration: const Duration(milliseconds: 200),
         width: 150,
         height: 33,
         decoration: BoxDecoration(
           border: Border.all(color: widget.color),
           borderRadius: BorderRadius.circular(8),
-          color: isSelected ? widget.color : Colors.white ,
+          color: isSelected ? widget.color : Colors.white,
         ),
         child: Center(
-          child: Text( 'N° ${bingo.number}',
-            style: TextStyle(color: isSelected ? Colors.white : widget.color,
-                fontWeight: FontWeight.bold, fontSize: 15),),
+          child: Text(
+            'N° ${bingo.number}',
+            style: TextStyle(
+                color: isSelected ? Colors.white : widget.color,
+                fontWeight: FontWeight.bold,
+                fontSize: 15),
+          ),
         ),
       ),
     );
   }
 
-  Widget _counter(){
+  Widget _counter() {
     return Material(
-      elevation: 1,
-      color: Colors.white,
-      borderRadius: BorderRadius.circular(8),
-      child: SizedBox(width: 320, height: 40, 
-        child: Row( mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-          const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Text('Cartones seleccionados', style: TextStyle(color: Colors.grey, fontSize: 14),),
+        elevation: 1,
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8),
+        child: SizedBox(
+          width: 320,
+          height: 40,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Cartones seleccionados',
+                  style: TextStyle(color: Colors.grey, fontSize: 14),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  '${selectedBingos.length.toString()} / ${widget.maxQuantity}',
+                  style: TextStyle(
+                      color: widget.color,
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text( '${selectedBingos.length.toString()} / ${widget.maxAmount}', style: TextStyle(color: widget.color, fontSize: 14, fontWeight: FontWeight.bold),),
-          ),
-        ],
-    ),)
-    );
+        ));
   }
 }
 
+enum Options {
+  manual,
+  random,
+}
+
+class BingoModel {
+  int id;
+  int number;
+
+  BingoModel({required this.id, required this.number});
+
+  int get gedId => id;
+  int get getNumber => number;
+}
 
 class CustomButton extends StatefulWidget {
   final String text;
@@ -326,12 +423,14 @@ class _CustomButtonState extends State<CustomButton> {
         color: Colors.transparent,
         child: InkWell(
           hoverColor:
-          widget.isEnabled ? widget.backgroundColor : Colors.transparent,
+              widget.isEnabled ? widget.backgroundColor : Colors.transparent,
           splashFactory: widget.isEnabled
               ? InkSplash.splashFactory
               : NoSplash.splashFactory,
           borderRadius: BorderRadius.circular(widget.height / 2),
-          onTap: () {_onTap();},
+          onTap: () {
+            _onTap();
+          },
           child: Center(
             child: Row(
               mainAxisSize: MainAxisSize.min,
